66801563a84f4ae36e2d2d6da65cede908ce0e1a
Chloe <chloe.fontaine@liip.ch>
2020-03-06 14:24:50 +0100

    (CHLOE-2) POST du formulaire avec TrainingSession et Exercice fonctionne

--- projet_muscu/muscu_site/views.py
+++ projet_muscu/muscu_site/views.py
@@ -4,7 +4,6 @@ from .models import TrainingSession, Exercice
 from .forms import SessionForm, ExerciceForm
 
 def session_creation(request):
-    initial_nb_of_row = range(1,4)
     session_form = SessionForm(request.POST)
     ExerciceFormSet = formset_factory(ExerciceForm, extra = 3)
     data = {
@@ -12,38 +11,31 @@ def session_creation(request):
         'form-INITIAL_FORMS': '0',
         'form-MAX_NUM_FORMS': '10',
     }
+    if request.method == 'POST':
+        exercice_formset = ExerciceFormSet(request.POST, request.FILES)
 
-    exercice_formset = ExerciceFormSet(data, request.POST, request.FILES)
-
-    if session_form.is_valid() and exercice_formset.is_valid():
-
-        TrainingSession.objects.create(
-            session_title=session_form.cleaned_data['session_title'],
-            date=session_form.cleaned_data['date'],
-        )
+        if session_form.is_valid() and exercice_formset.is_valid():
 
 
+            session = TrainingSession.objects.create(
+                session_title=session_form.cleaned_data['session_title'],
+                date=session_form.cleaned_data['date'],
+            )
 
 
-    # if request.method == 'POST':
-    #
-    #     session = TrainingSession.create(
-    #         label = request.POST.get('session_title'),
-    #         date = request.POST.get('creation_date')
-    #     )
-    #
-    #     exercices = Exercice.create(
-    #         training_session = session
-    #     )
-    #
-    #     for number in initial_nb_of_row:
-    #         session.exercice.label = request.POST.get('ex' + str(number))
-    #         session.exercice.sets = request.POST.get('set' + str(number))
-    #         session.exercice.reps = request.POST.get('rep' + str(number))
-    #         session.exercice.break_time = request.POST.get('bk' + str(number))
+            for ex in exercice_formset.forms:
+                if ex.cleaned_data:
+                    Exercice.objects.create(
+                        training_session = session,
+                        exercice = ex.cleaned_data['exercice'],
+                        sets = ex.cleaned_data['sets'],
+                        reps = ex.cleaned_data['reps'],
+                        break_time = ex.cleaned_data['break_time'],
+                    )
+    else:
+        exercice_formset = ExerciceFormSet()
 
     context = {
-        'nb_elements': initial_nb_of_row,
         'session_form': session_form,
         'exercice_formset': exercice_formset,
     }
