b0190d34235852b7474a0861fc1a2fc041ad8993
Chloe <chloe.fontaine@liip.ch>
2020-03-17 07:58:48 +0100

    (CHLOE-2-TEST): test for right cases added

--- projet_muscu/muscu_site/tests.py
+++ projet_muscu/muscu_site/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 from .forms import SessionForm, ExerciseForm
+from .models import TrainingSession, Exercise
 
 class SessionCreationTest(TestCase):
     def test_date_in_future(self):
@@ -21,3 +22,19 @@ class SessionCreationTest(TestCase):
             'date',
             'La date ne doit pas être dans le futur !'
         )
+
+    def test_date_in_past(self):
+        nb_instances_sessions = TrainingSession.objects.count()
+        self.client.post(reverse('create_session'), data={
+            'session_title': 'Ma Session',
+            'date': datetime.date.today(),
+            'exercise': 'exercice',
+            'sets': 3,
+            'reps': 12,
+            'time_break': 60,
+            'form-TOTAL_FORMS': '3',
+            'form-INITIAL_FORMS': '0',
+            'form-MAX_NUM_FORMS': '10',
+        })
+
+        self.assertEqual(TrainingSession.objects.count(), nb_instances_sessions)
